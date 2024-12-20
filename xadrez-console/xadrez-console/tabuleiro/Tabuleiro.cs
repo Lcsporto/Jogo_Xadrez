43eb3b08ec8c21eedbb73ae0bb62f7f8f169cb38
Lucas Lima <lucas_lp1@outlook.com.br>
2024-12-19 23:16:05 -0300

    Começando a Implementar o tabuleiro

---
+++ xadrez-console/xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,18 @@
+﻿
+namespace tabuleiro
+{
+    internal class Tabuleiro
+    {
+
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linha, int colunas)
+        {
+            this.linhas = linha;
+            this.colunas = colunas;
+            pecas = new Peca[linha, colunas];
+        }
+    }
+}
